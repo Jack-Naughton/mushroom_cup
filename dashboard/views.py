7cb75fbfa771fcde390c97937c185ec37d75099c
= <=>
2022-04-06 14:35:02 -0400

    transition to plotly approach

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,13 +1,19 @@
 from django.shortcuts import render
 
+import plotly.express as px
 import pandas as pd
 
 
 def dashboard(request):
-    # temps = pd.read_csv('dashboard/data/temp.csv')
-    return render(request, 'dashboard/index.html', {'temperatures': [
-        {'date': '2021-09-02', 'temp': 90},
-        {'date': '2021-09-03', 'temp': 91},
-        {'date': '2021-09-04', 'temp': 90},
-        {'date': '2021-09-05', 'temp': 94},
-    ]})
+    df = pd.read_csv('dashboard/data/temp.csv')
+
+    fig = px.line(df,
+                  x="date",
+                  y="temp",
+                  title='Temperature in Fahrenheit',
+                  labels={'date': 'Date', 'temp': 'Degrees Fahrenheit'}
+                  )
+
+    plot_div = fig.to_html(full_html=False, include_plotlyjs=False)
+
+    return render(request, "dashboard/index.html", context={'plot_div': plot_div})
