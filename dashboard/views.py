14e617f42b9bc6fbb0a33150d7304ffaa1304775
= <=>
2022-04-06 16:52:49 -0400

    add some color

--- dashboard/views.py
+++ dashboard/views.py
@@ -4,17 +4,25 @@ import plotly.express as px
 import pandas as pd
 
 
-def _render_chart(file, x, y, title, labels):
+def _render_chart(file, x, y, title, labels, plot_color=None):
     """ Helper method to render a plotly chart"""
 
     df = pd.read_csv(file)
+
+    df['date'] = pd.to_datetime(df['date'])
 
     fig = px.scatter(df,
                      x=x,
                      y=y,
                      title=title,
                      labels=labels,
+                     opacity=.3,
+                     trendline='rolling',
+                     trendline_options={'window': 300, 'center': True},
+                     trendline_color_override='darkred',
                      )
+
+    fig.update_traces(marker={'color': plot_color})
 
     mean = df[y].mean()
 
@@ -23,7 +31,6 @@ def _render_chart(file, x, y, title, labels):
         line_dash='dash',
         annotation_text='avg',
         annotation_font_size=20,
-        annotation_font_color='red',
     )
 
     fig.add_hline(
@@ -56,7 +63,8 @@ def temperature(request):
         x='date',
         y='temp',
         title='Temperature',
-        labels={'date': 'Date', 'temp': 'Degrees Fahrenheit'}
+        labels={'date': 'Date', 'temp': 'Degrees Fahrenheit'},
+        plot_color='blue',
     )
 
     return render(request, "dashboard/index.html", context={'chart': chart})
@@ -68,7 +76,7 @@ def co2(request):
         x='date',
         y='co2',
         title='CO2',
-        labels={'date': 'Date', 'co2': 'PPM'}
+        labels={'date': 'Date', 'co2': 'PPM'},
     )
 
     return render(request, "dashboard/index.html", context={'chart': chart})
@@ -80,7 +88,8 @@ def humidity(request):
         x='date',
         y='humidity',
         title='Humidity',
-        labels={'date': 'Date', 'humidity': '% Humidity'}
+        labels={'date': 'Date', 'humidity': '% Humidity'},
+        plot_color='green',
     )
 
     return render(request, "dashboard/index.html", context={'chart': chart})
