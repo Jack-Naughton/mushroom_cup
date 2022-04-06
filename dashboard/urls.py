db8439b5a121eb3749dc3b2ccf026d40bb6d1a34
= <=>
2022-04-06 14:43:55 -0400

    add basic views for co2 and humidity data

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.dashboard, name='dashboard'),
+    # path('', views.dashboard, name='dashboard'),
+    path('temperature', views.temperature, name='dashboard'),
+    path('co2', views.co2, name='dashboard'),
+    path('humidity', views.humidity, name='dashboard'),
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -4,14 +4,44 @@ import plotly.express as px
 import pandas as pd
 
 
-def dashboard(request):
+def temperature(request):
     df = pd.read_csv('dashboard/data/temp.csv')
 
     fig = px.line(df,
                   x="date",
                   y="temp",
-                  title='Temperature in Fahrenheit',
+                  title='Temperature',
                   labels={'date': 'Date', 'temp': 'Degrees Fahrenheit'}
+                  )
+
+    plot_div = fig.to_html(full_html=False, include_plotlyjs=False)
+
+    return render(request, "dashboard/index.html", context={'plot_div': plot_div})
+
+
+def co2(request):
+    df = pd.read_csv('dashboard/data/co2.csv')
+
+    fig = px.line(df,
+                  x="date",
+                  y="co2",
+                  title='CO2',
+                  labels={'date': 'Date', 'co2': 'PPM'}
+                  )
+
+    plot_div = fig.to_html(full_html=False, include_plotlyjs=False)
+
+    return render(request, "dashboard/index.html", context={'plot_div': plot_div})
+
+
+def humidity(request):
+    df = pd.read_csv('dashboard/data/humidity.csv')
+
+    fig = px.line(df,
+                  x="date",
+                  y="humidity",
+                  title='Humidity',
+                  labels={'date': 'Date', 'humidity': '% Humidity'}
                   )
 
     plot_div = fig.to_html(full_html=False, include_plotlyjs=False)
