4df345f735a8295c2b68dbce0ff09f3a1dc51bbc
= <=>
2022-04-06 12:51:32 -0400

    sample chart using d3 and observable

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 
+import pandas as pd
 
-# Create your views here.
+
 def dashboard(request):
-    pass
+    # temps = pd.read_csv('dashboard/data/temp.csv')
+    return render(request, 'dashboard/index.html', {'temperatures': [
+        {'date': '2021-09-02', 'temp': 90},
+        {'date': '2021-09-03', 'temp': 91},
+        {'date': '2021-09-04', 'temp': 90},
+        {'date': '2021-09-05', 'temp': 94},
+    ]})
