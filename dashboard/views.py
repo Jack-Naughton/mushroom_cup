d1b082826eb280b86b12228ff0bdb39e7517b202
= <=>
2022-04-06 21:40:08 -0400

    showcase outliers

--- dashboard/views.py
+++ dashboard/views.py
@@ -6,46 +6,58 @@ import pandas as pd
 
 def _render_chart(request, file, x, y, title, labels, plot_color=None):
     """ Helper method to render a plotly chart"""
-
     # read csv and parse date as date for trendline function
     df = pd.read_csv(file)
     df['date'] = pd.to_datetime(df['date'])
+
+    # add average and standard deviation lines
+    mean = df[y].mean()
+    std = df[y].std()
+
+    outlier_upper = mean + std * 3
+    outlier_lower = mean - std * 3
+
+    def is_outlier(z: int, bound_u: int, bound_l: int):
+        if z > bound_u or z < bound_l:
+            return 'y'
+        else:
+            return 'n'
+
+    df['is_outlier'] = [is_outlier(z=z, bound_u=outlier_upper, bound_l=outlier_lower) for z in df[y]]
 
     # generate scatterplot
     fig = px.scatter(df,
                      x=x,
                      y=y,
+                     color='is_outlier',
+                     color_discrete_sequence=[plot_color, 'darkred'],
                      title=title,
                      labels=labels,
                      opacity=.3,
                      trendline='rolling',
                      trendline_options={'window': 300, 'center': True},
-                     trendline_color_override='darkred',
+                     trendline_color_override='black',
                      )
 
-    # update node colors
-    fig.update_traces(marker={'color': plot_color})
+    fig.update_layout(showlegend=False)
 
-    # add average and standard deviation lines
-    mean = df[y].mean()
-    std = df[y].std()
     fig.add_hline(
         y=mean,
         line_dash='dash',
         annotation_text='avg',
-        annotation_font_size=20,
+        annotation_font_size=15,
     )
     fig.add_hline(
         y=mean + std,
         line_dash='dot',
         annotation_text='+1 std',
-        annotation_font_size=20,
+        annotation_font_size=10,
     )
     fig.add_hline(
         y=mean - std,
         line_dash='dot',
         annotation_text='-1 std',
-        annotation_font_size=20,
+        annotation_font_size=10,
     )
 
     chart = fig.to_html(full_html=False, include_plotlyjs=False)
